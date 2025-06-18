2c81a6e03cec0bfb0180f863fbe833073b65674e
Elijah Mejilla <111504836+jgmejilla@users.noreply.github.com>
2025-06-18 14:23:11 +0800

    finish navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,33 @@
 <script>
+  import "../app.css"
+  import { page } from "$app/state";
+	import { onMount } from "svelte";
   let { children } = $props();
-  import "../app.css";
+
+
+
 </script>
 
-<div class="flex h-screen">
-  <aside class="w-72 bg-white border-r-1 border-r-gray-200  flex flex-col py-10 px-6">
-    <h1 class="text-2xl font-bold mb-10 text-gray-800 font-display">Accounting</h1>
-    <nav class="flex flex-col gap-1">
-        {#each [
+<div class="flex h-screen font-display font-semibold">
+    <aside class="w-72 bg-bg text-display flex flex-col bg-[#f1f5f2]/50 px-4 py-4">
+        <span class="text-md mb-4">
+          Menu
+        </span>
+        <nav class="w-full text-sm flex flex-col gap-1">
+          {#each [
             {name: "Vouchers", link: "/vouchers"},
             {name: "Projects", link: "/projects"},
-            {name: "History", link: "/history"}
-        ] as item}
-        <a href={item.link} class="flex items-center px-4 py-4 rounded-lg text-gray-700 hover:underline hover:bg-gray-100 hover:text-blue-600 transition">
+            {name: "History", link: "/history"},
+            {name: "Reports", link: "/reports"},
+          ] as item}
+          <a href={item.link} class="block w-full py-3 px-5 rounded-full {page.url.pathname === item.link ? "bg-secondary/50" : "hover:bg-gray-200/80"}">        
             {item.name}
-        </a>
-        {/each}
-      
-    </nav>
-  </aside>
-
-  <main class="h-screen w-full p-10">
-    {@render children()}
-  </main>
-  
+          </a>
+          {/each}
+        </nav>
+    </aside>
+    <main class="flex-1 px-16 py-16">
+        {@render children()}
+    </main>
 </div>
-
-
-
 
--- src/routes/+page.server.js
+++
@@ -1,9 +0,0 @@
-import { supabase } from "$lib/supabaseClient";
-  export async function load() {
-    const { data } = await supabase
-        .from("tests")
-        .select()
-        .single();
-
-    return data;
-  }
