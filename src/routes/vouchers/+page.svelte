675821187e25f7f8190b067a5d8d3fc20325bc00
Elijah Mejilla <111504836+jgmejilla@users.noreply.github.com>
2025-06-27 15:42:04 +0800

    reformat common info

--- src/routes/vouchers/+page.svelte
+++ src/routes/vouchers/+page.svelte
@@ -62,13 +62,13 @@
   selectedProject = projects[0]
 </script>
 
-<div class="flex justify-between items-center gap-5">
-  <div class="flex justify-start items-center gap-2">
+<div class="flex justify-between items-start gap-5">
+  <div class="flex justify-start items-center gap-3">
     <div class="flex gap-3 items-center">
       <h1 class="text-4xl font-semibold">
         <a href="#top">Add Vouchers</a> 
       </h1> 
-      <span class="ml-0.5 -mr-0.5">to</span>
+      <span class="ml-0.5 -mb-0.5 text-sm">to</span>
 
       <select bind:value={selectedProject} id="countries" class=" bg-primary/10
       text-sm rounded-lg border-2 border-primary hover:bg-primary/20
@@ -80,27 +80,43 @@
       </select>
     </div>
 
-    <div style="dipslay: flex; gap: 3px;">
-      <button onclick={addRow} class="">add row</button>
-      <button onclick={generateAllVouchers} class="">Generate All Vouchers</button>
+    <div class="py-3 text-black/30">•</div>
+
+    <div style="">
+      <button
+            type="button"
+            class="border text-white bg-green-800 hover:bg-green-900 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5"
+            onclick={addRow}
+      >
+        Add Row
+      </button>
+      <button
+            type="button"
+            class="border text-white bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5"
+            onclick={generateAllVouchers}
+      >
+        Generate All Vouchers
+      </button>
     </div>
   </div>
 
-  
-
-  
-
-
+ 
 </div>
   
 
-  <div style="display: flex; gap: 3px">
-    <span>authorized rep: </span> <input bind:value={authorizedRep} type="text">
-  </div>
-  <div style="display: flex; gap: 3px">
-    <span>approver: </span> <input bind:value={approver} type="text">
+<div class="my-2 gap-2 flex items-center">
+  <div class="flex justify-start gap-2 pt-1">
+    <span class="font-medium text-lg">Authorized representative: </span> 
+    <input bind:value={authorizedRep} class="border" type="text">
   </div>
 
+  <div class="text-black/30">•</div>
+
+  <div class="flex justify-start gap-2 pt-1">
+    <span class="font-medium text-lg">Approver: </span> 
+    <input bind:value={approver} class="border" type="text">
+  </div>
+</div>
 <!--
   ===============
   TABLE
